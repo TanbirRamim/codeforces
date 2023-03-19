77cfc8f751538b69e46bafbcb775d84e37aec596
Your Tech Guy <tanbirramim420@gmail.com>
2023-03-20 02:46:43 +0600

    eksho bochor pore 00 complexity er problem checking

---
+++ randomproblems/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b == c)
+        {
+            cout << "+" << endl;
+        }
+        else
+            cout << "-" << endl;
+    }
+    return 0;
+}
